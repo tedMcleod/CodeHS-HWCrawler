180d308215d36611ccd351b99c27e8e3a44f8ced
e-zhang09 <36081769+e-zhang09@users.noreply.github.com>
2019-11-28 21:13:47 -0800

    concurrency enabled

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const pLimit = require('p-limit');
+const limit = pLimit(2); //TODO: Change this to be based on cpu cores
+const puppeteer = require('puppeteer');
+const CREDS = require('./secrets/creds');
+const SECTIONS = require('./secrets/sections');
+const TEMPLATE_HOME_URL = 'https://codehs.com/lms/assignments/{0}/section/{1}/progress/module/0';
+const format = require('string-format');
+
 console.log('loaded index.js');
 let arr_args = process.argv.slice(2);
 
 if(arr_args.length === 0){
-    arr_args = ['11/11/2019', '12:12', 'A,', 'AB,', 'ABC,', 'ABCD,', 'ABCDE']
+    arr_args = [ '11/22/19', '11:11', '2', 'Mindsets', 'Short Stack', '2', 'm0', 's0' ];
 }
 
 // console.log('arguments', arr_args);
@@ -10,22 +18,50 @@ if(arr_args.length === 0){
 let arr_dueDate = arr_args[0].split('/');
 let arr_dueTime = arr_args[1].split(':');
 
-let date_dueDate = new Date(+arr_dueDate[2], +arr_dueDate[0], +arr_dueDate[1], +arr_dueTime[0], +arr_dueTime[0], 0, 0);
+let date_dueDate = new Date((arr_dueDate[2].length !== 4 ? 2000 + (+arr_dueDate[2]): +arr_dueDate[2]),
+    +arr_dueDate[0], +arr_dueDate[1], +arr_dueTime[0], +arr_dueTime[0], 0, 0);
 
-let assignmentsCount = arr_args[2];
+let assignmentsCount = +arr_args[2];
 let arr_assignments = [];
-for(let i = 0; i < +assignmentsCount; i ++){
+for(let i = 0; i < assignmentsCount; i ++){
     arr_assignments.push(arr_args[3 + i]);
+}
+
+let arr_objs_classes = [];
+let teachersCount = +arr_args[3 + assignmentsCount];
+for (let i = 0; i < teachersCount; i++) {
+    let rawTeacherStr = arr_args[4 + assignmentsCount + i];
+    let teacherInitial = rawTeacherStr.charAt(0);
+    let teacherName = SECTIONS[teacherInitial].name;
+    let teacherStr = rawTeacherStr.substring(1);
+    teacherStr.trim().split('').forEach(char => {
+        if(char === '0'){
+            //0 --> All classes
+            for(let classNum in SECTIONS[teacherInitial].classes) {
+                if (!SECTIONS[teacherInitial].classes.hasOwnProperty(classNum)) continue;
+                let obj_todo = {
+                    teacherName : teacherName,
+                    url: format(TEMPLATE_HOME_URL, SECTIONS[teacherInitial].id, SECTIONS[teacherInitial].classes[classNum])
+                };
+                arr_objs_classes.push(obj_todo);
+            }
+        }else{
+            let classNum = +char;
+            let obj_todo = {
+                teacherName : teacherName,
+                url: format(TEMPLATE_HOME_URL, SECTIONS[teacherInitial].id, SECTIONS[teacherInitial].classes[classNum])
+            };
+            arr_objs_classes.push(obj_todo);
+        }
+    })
 }
 
 console.info(date_dueDate);
 console.info(arr_assignments);
 
+start().then();
 
-const puppeteer = require('puppeteer');
-const CREDS = require('./creds');
-
-(async () => {
+async function start(){
     const browser = await puppeteer.launch({
         headless: false
     });
@@ -47,8 +83,27 @@ const CREDS = require('./creds');
     await page.waitForNavigation();
 
 
+    let temp = arr_objs_classes.slice(5); //TODO: Change this to actual array
+    let input = [];
+    temp.forEach(obj => {
+        input.push(parsePage(obj, page))
+    });
 
+    (async () => {
+        // Only one promise is run at once
+        const result = await Promise.all(input);
+        console.log(result);
+    })();
 
-    // await page.screenshot({path: 'example.png'});
     await browser.close();
-})();
+}
+
+
+async function parsePage(obj, page){
+    return new Promise(async(resolve, reject) => {
+
+
+
+        resolve(obj);
+    })
+}
